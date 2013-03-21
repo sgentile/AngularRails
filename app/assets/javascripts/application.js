ccf3b0bd5df406d9f57915d8f6888b61ac9e0ee4
Steve Gentile <steven.gentile@gmail.com>
2013-03-21 10:56:23 -0400

    added twitter bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,7 @@ angular.module('AngularRails', ['moviesService'])
   .config(['$routeProvider', function($router){
     $router
       .when('/movies', {templateUrl:'/movies/index.html', controller:MoviesCtrl})
+      .when('/movies/add', {templateUrl:'/movies/add.html', controller: MovieAddCtrl})
       .when('/movies/:movie_id', {templateUrl:'/movies/show.html', controller:MovieShowCtrl})
       .when('/movies/:movie_id/edit', {templateUrl:'/movies/edit.html', controller: MovieEditCtrl})
       .otherwise({redirectTo: '/movies'});
