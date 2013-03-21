44757a983c2831680d55cb795865313ae267cd1d
Steve Gentile <steven.gentile@gmail.com>
2013-03-21 08:17:36 -0400

    created a 'show movies' route

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,8 @@
 angular.module('AngularRails', ['moviesService'])
   .config(['$routeProvider', function($router){
     $router
-      .when('/movies/show', {templateUrl:'/movies/show.html', controller:MoviesCtrl})
-      .otherwise({redirectTo: '/movies/show'});
+      .when('/movies', {templateUrl:'/movies/index.html', controller:MoviesCtrl})
+      .when('/movies/:movie_id', {templateUrl:'/movies/show.html', controller:MovieShowCtrl})
+      .when('/movies/:movie_id/edit', {templateUrl:'/movies/edit.html', controller: MovieEditCtrl})
+      .otherwise({redirectTo: '/movies'});
   }]);
