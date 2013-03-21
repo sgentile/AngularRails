e4154c5ad526345a7ab72b42727af8b21ac6b370
Steve Gentile <steven.gentile@gmail.com>
2013-03-21 08:50:04 -0400

    created edit for movies

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,8 +3,26 @@ class MovieController < ApplicationController
   def index 
     respond_with(@movies = Movie.all)
   end
+
   def show
     @movie = Movie.find(params[:id])
     respond_with(@movie)
+  end
+
+  def update 
+    @movie = Movie.find(params[:id])
+    #if @movie.update_attributes(params[:movie])
+      #respond.with({head :no_content})
+      
+   # else
+      #respond_with({render json: @movie.errors, status: :unprocessable_entity }
+    #end
+    respond_to do |format|
+      if @movie.update_attributes(params[:movie])
+        format.json { head :no_content }
+      else
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
